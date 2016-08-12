810663a281a49427422c7cf6cb3fbfdce6d93e07
alex cai <caiyingyao@ibbd.net>
2016-08-12 15:49:44 +0800

    run sh

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@
 # Created Time: 2016年06月27日 星期一 10时34分36秒
 
 docker run -ti --name=ibbd-go -d \
-    -v /var/www:/var/www \
-    -v /var/www/go-src/:/go \
+    -v /var/www/golang/src/git.ibbd.net:/var/www \
+    -v /var/www/golang:/go \
     -p 8080:8080 \
+    -p 8081:8081 \
+    -p 8082:8082 \
     golang \
     /bin/bash
